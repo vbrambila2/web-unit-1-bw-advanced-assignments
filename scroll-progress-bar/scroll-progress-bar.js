7f2b9c0a2b8b3f5fe0a754e8c07faf44cf845b51
Keiran Kozlowski <keirankozlowski@gmail.com>
2022-01-30 20:42:17 -0500

    adding solution code for progress bar

--- scroll-progress-bar/scroll-progress-bar.js
+++ scroll-progress-bar/scroll-progress-bar.js
@@ -1,1 +1,13 @@
+let topp = window.scrollY;
+let vh = document.body.clientHeight - window.innerHeight;
+let percent = (topp / vh) * 100;
 
+let progressBar = document.querySelector('.progress');
+
+window.addEventListener('scroll', function() {
+    console.log('hello');
+    topp = window.scrollY;
+    vh = document.body.clientHeight - window.innerHeight;
+    percent = (topp / vh) * 100;
+    progressBar.style.width = percent + '%';
+});
